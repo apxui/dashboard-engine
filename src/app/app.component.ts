3c5c13740c71f4983b17e2ff5faaa8f610a46722
Lucas <lucas.feng@advent.com>
2018-04-08 19:48:29 -0700

    init project

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-root',
+	template: `
+        <h1>Dashboard Engine</h1>
+	`,
+	styles: []
+})
+export class AppComponent {
+
+}
