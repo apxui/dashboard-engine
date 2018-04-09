58d739f3938e42347665e440c75c01f5808c80d5
Lucas <lucas.feng@advent.com>
2018-04-09 01:56:39 -0700

    added test chart

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,42 @@
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import * as echarts from 'echarts';
+
+@Component({
+	selector: 'app-test',
+	template: `
+		Test
+		<div [id]="id" style="width: 600px;height:400px;"></div>
+	`
+})
+export class TestComponent implements AfterViewInit {
+	@Input() id: string;
+
+	constructor() {}
+
+	ngAfterViewInit(): void {
+		let myChart = echarts.init(document.getElementById(this.id));
+
+		// 指定图表的配置项和数据
+		let option = {
+			title: {
+				text: 'ECharts 入门示例'
+			},
+			tooltip: {},
+			legend: {
+				data:['销量']
+			},
+			xAxis: {
+				data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+			},
+			yAxis: {},
+			series: [{
+				name: '销量',
+				type: 'bar',
+				data: [5, 20, 36, 10, 10, 20]
+			}]
+		};
+
+		// 使用刚指定的配置项和数据显示图表。
+		myChart.setOption(option);
+	}
+}
