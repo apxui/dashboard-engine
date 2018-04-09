865a42af4e19c6c5f86f63b7c976a5f1af725970
Lucas <lucas.feng@advent.com>
2018-04-09 04:06:08 -0700

    added navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
 	selector: 'app-root',
 	template: `
-        <h1>Dashboard Engine</h1>
-		<app-test></app-test>
+        <nav class="navbar navbar-expand sticky-top navbar-dark bg-primary d-flex justify-content-between">
+            <a class="navbar-brand" href="#">Dashboard Engine</a>
+
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="https://github.com/apxui/dashboard-engine" target="_blank"><i class="fab fa-github fa-lg fa-fw fa-spin mr-1"></i>Follow us on GitHub</a>
+                </li>
+            </ul>
+        </nav>
+        
+        <div class="container-fluid">
+	        <app-test></app-test>
+        </div>
 	`
 })
 export class AppComponent {
