5de15076cdc740b1d7fafade64cdd5588103c277
Lucas <lucas.feng@advent.com>
2018-04-10 02:02:39 -0700

    added main buttons

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -3,11 +3,23 @@ import { Component } from '@angular/core';
 @Component({
 	selector: 'app-main',
 	styleUrls: ['./main.component.scss'],
-	template: `		
-        <div class="container-fluid">
-			Main
-        </div>
+	template: `
+		<div class="sticky-top">
+            <div class="container-fluid">
+                <div class="row col-12">
+	                <h6>Begin with a data type:</h6>
+                </div>
+                <div class="row col-12">
+	                <div class="col-md-2 col-sm-3"
+	                     *ngFor="let e of supportedEntities"
+	                >
+                        <button class="entity-button btn btn-outline-secondary">{{e}}</button>
+	                </div>
+                </div>
+            </div>
+		</div>
 	`
 })
 export class MainComponent {
+	supportedEntities: string[] = ['Portfolios', 'Activities', 'Contacts', 'Holdings', 'Transactions', 'Securities'];
 }
