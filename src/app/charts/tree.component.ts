4dee9f96a93bea93991a0bed9a306db65ecc4687
ADVENT\gzhao <gzhao@advent.com>
2018-04-09 20:35:02 -0700

    grace tree click handler

--- src/app/charts/tree.component.ts
+++ src/app/charts/tree.component.ts
@@ -3,18 +3,28 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
     selector: 'chart-tree',
     template: `
-        <div echarts [options]="_options"></div>
+        <div echarts [options]="_options.option" (chartClick)="onChartClick($event)"></div>
 	`
 })
 export class TreeComponent implements OnInit {
     _options: any;
-    @Input() set options(optionValue: any) {
+    @Input() set options(optionValue: TreeOption) {
         this._options = optionValue;
     }
 
     ngOnInit(): void {
         console.log(this.options);
     }
+
+    onChartClick(e: any): void{
+        if (this._options && this._options.onTreeClickHandler) {
+            this._options.onTreeClickHandler(e.data);
+        }
+    }
+}
+export class TreeOption {
+    option: any;
+    onTreeClickHandler: (treeNodeData: any) => {};
 }
 export class TreeData {
     name: string;
