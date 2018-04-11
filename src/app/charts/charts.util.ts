eac0befe1eae7f097baaf9a1ab5e577496bd6e92
ADVENT\gzhao <gzhao@advent.com>
2018-04-11 06:34:25 -0700

    5

--- src/app/charts/charts.util.ts
+++ src/app/charts/charts.util.ts
@@ -99,7 +99,7 @@ export class ChartsUtil {
 			tooltip : barTooltip,
             
             visualMap: {
-                max: 20,
+                max: 100,
                 inRange: {
                     color: ['#313695', '#4575b4', '#74add1', '#abd9e9', '#e0f3f8', '#ffffbf', '#fee090', '#fdae61', '#f46d43', '#d73027', '#a50026']
                 }
